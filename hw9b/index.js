4a980917241e149b5a59d7f0a0a3702b17d0a35e
IrisWu <xianwu_iris@126.com>
2021-04-13 22:18:56 +0800

    change

--- hw9b/index.js
+++ hw9b/index.js
@@ -1,8 +1,11 @@
-// Load the Express package as a module
+// Load the Express and Multer package as a module
 const express = require("express");
+const multer = require("multer");
 
 // Access the exported service
 const app = express();
+const upload = multer();
+
 
 app.use(express.static("public"));
 app.use(express.static("css"));
@@ -22,6 +25,18 @@ app.use((req, res, next) => {
 app.get("/", (request, response) => {
     response.sendFile(`${__dirname}/views/index.html`);
 });
+
+app.get("/ex1", (request, response) => {
+    response.sendFile(`${__dirname}/views/ex1.html`);
+});
+
+app.post("/ex1", upload.array(), (request, response) => {
+    console.log(request.body);
+    const name = request.body.name;
+    const email = request.body.email;
+    response.send(`${name}, thank you for your order. We will keep you posted on delivery status at ${email}`);
+  });
+
 
 // Start listening to incoming requests
 // If process.env.PORT is not defined, port number 3000 is used
