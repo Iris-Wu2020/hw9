9d126462698617cee71895959806610d9c27cea3
IrisWu <xianwu_iris@126.com>
2021-04-14 09:58:36 +0800

    change

--- hw9b/public/ex2.js
+++ hw9b/public/ex2.js
@@ -1,18 +1,32 @@
+const countries = {
+    name: "Iris",
+    countries: [
+        {
+            name: "UK",
+            year: 2017
+        },
+        {
+            name: "USA",
+            year: 2019
+        }
+    ]
+};
+
 document.getElementById("countries").addEventListener("click", e => {
-    fetch("/api/", {
+    fetch("/api/countries", {
         method: "POST",
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(travelInformation)
+        body: JSON.stringify(countries)
     })
         .then(response => response.text())
         .then(result => {
-            console.log(result);
+            document.getElementById("result").textContent = result;
         })
         .catch(err => {
             console.error(err.message);
         });
 
-})
+});
