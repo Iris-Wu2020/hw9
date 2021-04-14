0a9f336a13d18f19b163572cc8fbba806199e387
IrisWu <xianwu_iris@126.com>
2021-04-14 09:54:27 +0800

    change

--- hw9b/public/ex3.js
+++ hw9b/public/ex3.js
@@ -2,7 +2,7 @@ document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault();
     const formData = new FormData(e.target);
     // Send form data to the server with an asynchronous POST request
-    fetch("http://localhost:3000/ex1", {
+    fetch("http://localhost:3000/ex3", {
         method: "POST",
         body: formData
     })
